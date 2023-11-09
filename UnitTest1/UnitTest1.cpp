aa657876bde47146d66aeb49cfc7568057a155f4
olia0973469 <Olha.Tykha.RI.2023@lpnu.ua>
2023-11-09 06:24:16 +0200

    second test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project5.1.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = f(2);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
